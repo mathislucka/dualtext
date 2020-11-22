850639443a0cf00bb4bde78ef108fcecc23e8376
mathis <mathis.lucka@gmail.com>
2020-11-22 17:37:45 +0100

    change sentence embeddings

--- dualtext_api/feature_builders/sentence_embedding.py
+++ dualtext_api/feature_builders/sentence_embedding.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import pickle
 
-model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+model = SentenceTransformer('distilbert-multilingual-nli-stsb-quora-ranking')
 
 class SentenceEmbedding():
     def __init__(self):
@@ -12,15 +12,22 @@ class SentenceEmbedding():
         #Our sentences we like to encode
         sentences = [document.content for document in documents.all()]
         ids = [document.id for document in documents.all()]
+        sentences = self.split_list(sentences, 4)
 
-        #Sentences are encoded by calling model.encode()
-        embeddings = self.model.encode(sentences, convert_to_tensor=True)
-        embeddings = [pickle.dumps(ndarray, protocol=None, fix_imports=True, buffer_callback=None) for ndarray in embeddings]
-
-
-        return zip(ids, embeddings)
+        to_store = []
+        for lst in sentences:
+            embeddings = self.model.encode(lst, convert_to_tensor=True)
+            to_store.extend([pickle.dumps(ndarray, protocol=None, fix_imports=True, buffer_callback=None) for ndarray in embeddings])
+        
+        return zip(ids, to_store)
 
     def process_query(self, query):
         embeddings = self.model.encode([query])
         return embeddings[0]
+    
+    def split_list(self, lst, parts):
+        length = len(lst)
+        return [ lst[i*length // parts: (i+1)*length // parts] 
+                for i in range(parts) ]
+
 
