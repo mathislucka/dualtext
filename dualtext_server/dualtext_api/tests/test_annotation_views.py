85bced4f0d45e91ced8d8d3488c44cbf94d6d91e
mathis <mathis.lucka@gmail.com>
2021-01-30 23:53:11 +0100

    refactor annotation tests

--- dualtext_server/dualtext_api/tests/test_annotation_views.py
+++ dualtext_server/dualtext_api/tests/test_annotation_views.py
@@ -1,173 +1,128 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
-from dualtext_api.models import Task, Corpus, Document, Annotation, Label, Run, Lap
-from .helpers import run_standard_setup
+from dualtext_api.models import Annotation, Run, Lap
+from .factories import AnnotationFactory, DocumentFactory, TaskFactory, UserFactory, LabelFactory
 import datetime
 
 class TestAnnotationListView(APITestCase):
-    def setUp(self):
-        standards = run_standard_setup()
-        self.user = standards['user']
-        self.superuser = standards['superuser']
-        self.group = standards['group']
-        self.project = standards['project']
-        task = Task(name='Task 1', annotator=self.user, project=self.project)
-        task.save()
-        self.task = task
-
-        corpus = Corpus(name='New Corpus', corpus_meta={})
-        corpus.save()
-        self.corpus = corpus
-
-        document = Document(content='A new document', corpus=self.corpus)
-        document.save()
-        self.document = document
-
-        self.url = reverse('annotation_list', args=[self.task.id])
-    
     def test_creation(self):
         """
         Ensure a new annotation can be created by a superuser.
         """
-        data = {'documents': [self.document.id], 'corpus': [self.corpus.id]}
-        self.client.force_authenticate(user=self.superuser)
-        response = self.client.post(self.url, data, format='json')
+        su = UserFactory(is_superuser=True)
+        task = TaskFactory()
+        doc = DocumentFactory()
+        data = {'documents': [doc.id]}
+        url = reverse('annotation_list', args=[task.id])
+        
+        self.client.force_authenticate(user=su)
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Annotation.objects.count(), 1)
-        self.assertEqual(Annotation.objects.get(id=1).documents.all()[0], self.document)
-        self.assertEqual(Annotation.objects.get(id=1).task, self.task)
+        self.assertEqual(Annotation.objects.get(id=1).documents.all()[0], doc)
+        self.assertEqual(Annotation.objects.get(id=1).task, task)
 
     def test_superuser_view(self):
         """
         Ensure a superuser can view all annotations.
         """
-        a1 = Annotation(task=self.task)
-        a1.save()
-        a1.documents.add(self.document)
-        a1.save()
+        su = UserFactory(is_superuser=True)
+        task = TaskFactory()
+        doc = DocumentFactory()
+        anno = AnnotationFactory(documents=[doc], task=task)
+        url = reverse('annotation_list', args=[task.id])
 
-        self.client.force_authenticate(user=self.superuser)
-        response = self.client.get(self.url, format='json')
+        self.client.force_authenticate(user=su)
+        response = self.client.get(url, format='json')
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['documents'], [self.document.id])
-        self.assertEqual(response.data[0]['task'], self.task.id)
+        self.assertEqual(response.data[0]['documents'], [doc.id])
+        self.assertEqual(response.data[0]['id'], anno.id)
 
     def test_view_only_allowed(self):
         """
         Ensure users can only view annotations that are children of tasks where the user is assigned as annotator.
         """
-        t2 = Task(annotator=self.user, project=self.project, name='Assigned Task')
-        t2.save()
-        t3 = Task(annotator=self.superuser, project=self.project, name='Not assigned task')
-        t3.save()
+        user = UserFactory()
+        su = UserFactory(is_superuser=True)
+        task_1 = TaskFactory(annotator=user)
+        task_2 = TaskFactory(annotator=su)
+        anno_1 = AnnotationFactory(task=task_1)
+        AnnotationFactory(task=task_2)
+        url_1 = reverse('annotation_list', args=[task_1.id])
+        url_2 = reverse('annotation_list', args=[task_2.id])
 
-        a1 = Annotation(task=self.task)
-        a1.save()
-        a1.documents.add(self.document)
-        a1.save()
+        self.client.force_authenticate(user=user)
+        response_1 = self.client.get(url_1, format='json')
+        response_2 = self.client.get(url_2, format='json')
 
-        a2 = Annotation(task=t2)
-        a2.save()
-        a2.documents.add(self.document)
-        a2.save()
-
-        a3 = Annotation(task=t3)
-        a3.save()
-        a3.documents.add(self.document)
-        a3.save()
-
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.url, format='json')
-        response_2 = self.client.get(reverse('annotation_list', args=[t2.id]))
-        response_3 = self.client.get(reverse('annotation_list', args=[t3.id]))
-
-        response_2_task = Task.objects.get(id=response_2.data[0]['task'])
-        response_task = Task.objects.get(id=response.data[0]['task'])
-
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response_task.annotator, self.user)
-
-        self.assertEqual(len(response_2.data), 1)
-        self.assertEqual(response_2_task.annotator, self.user)
-
-        self.assertEqual(len(response_3.data), 0)
+        self.assertEqual(len(response_1.data), 1)
+        self.assertEqual(response_1.data[0]['id'], anno_1.id)
+        self.assertEqual(len(response_2.data), 0)
 
     def test_deny_creation_non_superuser(self):
         """
         Ensure only superuser can create annotations.
         """
-        self.client.force_authenticate(user=self.user)
+        user = UserFactory()
+        task = TaskFactory()
+        url = reverse('annotation_list', args=[task.id])
 
-        response = self.client.post(self.url, {}, format='json')
+        self.client.force_authenticate(user=user)
+        response = self.client.post(url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_deny_not_authenticated(self):
         """
         Ensure only authenticated users can see annotations.
         """
-        response = self.client.get(self.url, format='json')
+        task = TaskFactory()
+        url = reverse('annotation_list', args=[task.id])
+
+        response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 class TestAnnotationDetailView(APITestCase):
-    def setUp(self):
-        standards = run_standard_setup()
-        self.user = standards['user']
-        self.superuser = standards['superuser']
-        self.group = standards['group']
-        self.project = standards['project']
-
-        task = Task(name='Task 1', annotator=self.user, project=self.project)
-        task.save()
-        self.task = task
-
-        corpus = Corpus(name='New Corpus', corpus_meta={})
-        corpus.save()
-        self.corpus = corpus
-
-        document = Document(content='A new document', corpus=self.corpus)
-        document.save()
-        self.document = document
-
-        annotation = Annotation(task=self.task)
-        annotation.save()
-        annotation.documents.add(self.document)
-        annotation.save()
-        self.annotation = annotation
-
-        self.url = reverse('annotation_detail', args=[self.annotation.id])
-
     def test_annotator_view(self):
         """
         Ensure that annotators of a task can view an annotation.
         """
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.url, format='json')
-        response_task_id = response.data['task']
+        user = UserFactory()
+        task = TaskFactory(annotator=user)
+        anno = AnnotationFactory(task=task)
+        url = reverse('annotation_detail', args=[task.id])
 
-        self.assertEqual(response_task_id, self.task.id)
-        self.assertEqual(Task.objects.get(id=response_task_id).annotator, self.user)
+        self.client.force_authenticate(user=user)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], anno.id)
 
     def test_superuser_view(self):
         """
         Ensure that superusers can see all annotations.
         """
-        self.client.force_authenticate(user=self.superuser)
-        response = self.client.get(self.url, format='json')
-        self.assertEqual(response.data['id'], self.annotation.id)
-        self.assertEqual(response.data['task'], self.task.id)
+        su = UserFactory(is_superuser=True)
+        task = TaskFactory()
+        anno = AnnotationFactory(task=task)
+        url = reverse('annotation_detail', args=[task.id])
+
+        self.client.force_authenticate(user=su)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], anno.id)
 
     def test_annotator_edit(self):
         """
         Ensure that annotators can edit an annotation.
         """
-        label = Label(
-            name="annotator",
-            project=self.project, color={'standard': '#97C0E8', 'light': '#EAF2FA'}
-        )
-        label.save()
-        self.client.force_authenticate(user=self.user)
-        response = self.client.patch(self.url, {'labels': [label.id]}, format='json')
+        user = UserFactory()
+        task = TaskFactory(annotator=user)
+        AnnotationFactory(task=task)
+        label = LabelFactory()
+        url = reverse('annotation_detail', args=[task.id])
+
+        self.client.force_authenticate(user=user)
+        response = self.client.patch(url, {'labels': [label.id]}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Annotation.objects.get(id=response.data['id']).labels.all()[0], label)
 
@@ -175,64 +130,78 @@ class TestAnnotationDetailView(APITestCase):
         """
         Ensure that superusers can edit an annotation.
         """
-        label = Label(name="annotator", project=self.project, color={'standard': '#97C0E8', 'light': '#EAF2FA'})
-        label.save()
+        su = UserFactory(is_superuser=True)
+        task = TaskFactory(annotator=su)
+        AnnotationFactory(task=task)
+        label = LabelFactory()
+        url = reverse('annotation_detail', args=[task.id])
 
-        self.client.force_authenticate(user=self.superuser)
-        response = self.client.patch(self.url, {'labels': [label.id]}, format='json')
+        self.client.force_authenticate(user=su)
+        response = self.client.patch(url, {'labels': [label.id]}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Annotation.objects.get(id=response.data['id']).labels.all()[0], label)
 
     def test_deny_non_annotator_view(self):
         """
-        Ensure that users who are neither annotator nor reviewer can't see an annotation.
+        Ensure that users who are neither annotator nor a superuser can't see an annotation.
         """
-        self.task.annotator = self.superuser
-        self.task.save()
+        user = UserFactory()
+        anno = AnnotationFactory()
+        url = reverse('annotation_detail', args=[anno.task.id])
 
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.url, format='json')
+        self.client.force_authenticate(user=user)
+        response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_deny_non_annotator_edit(self):
         """
-        Ensure that users who are neither annotator nor reviewer can't edit an annotation.
+        Ensure that users who are neither annotator nor a superuser can't edit an annotation.
         """
-        self.task.annotator = self.superuser
-        self.task.save()
+        user = UserFactory()
+        anno = AnnotationFactory()
+        url = reverse('annotation_detail', args=[anno.task.id])
 
-        self.client.force_authenticate(user=self.user)
-        response = self.client.patch(self.url, {}, format='json')
+        self.client.force_authenticate(user=user)
+        response = self.client.patch(url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_timetracking_on_update(self):
         """
-        Ensure that a timetracking Lap and possibly a timetracking run is created when the label or documents of
-        an annotation get an update.
+        Ensure that a timetracking Lap and possibly a timetracking run is created when the
+        label or documents of an annotation get an update.
         """
-        label = Label(name="annotator", project=self.project, color={'standard': '#97C0E8', 'light': '#EAF2FA'})
-        label.save()
-        self.client.force_authenticate(user=self.user)
-        self.client.patch(self.url, {'labels': [label.id]}, format='json')
+        su = UserFactory(is_superuser=True)
+        anno = AnnotationFactory()
+        label = LabelFactory()
+        url = reverse('annotation_detail', args=[anno.task.id])
+        
+        self.client.force_authenticate(user=su)
+        self.client.patch(url, {'labels': [label.id]}, format='json')
+
         run = Run.objects.all()
         lap = Lap.objects.all()
-
         self.assertEqual(len(run), 1)
-        self.assertEqual(len(lap), 2)
+        self.assertEqual(len(lap), 1)
 
     def test_new_run_when_idle(self):
         """
-        Ensure that a timetracking Lap is assigned to a newly created Run if the last Run was idle for more than
-        300 seconds.
+        Ensure that a timetracking Lap is assigned to a newly created Run if the last Run
+        was idle for more than 300 seconds.
         """
-        label = Label(name="annotator", project=self.project, color={'standard': '#97C0E8', 'light': '#EAF2FA'})
-        label.save()
+        su = UserFactory(is_superuser=True)
+        anno = AnnotationFactory()
+        label = LabelFactory()
+        url = reverse('annotation_detail', args=[anno.task.id])
+
+        self.client.force_authenticate(user=su)
+        self.client.patch(url, {'labels': [label.id]}, format='json')
+
         now = datetime.datetime.now()
         idle = now - datetime.timedelta(seconds=301)
         run = Run.objects.all().first()
-        run.lap_set.filter(id=1).update(created_at=idle, run_id=run.id, annotation_id=self.annotation.id)
-        self.client.force_authenticate(user=self.user)
-        self.client.patch(self.url, {'labels': [label.id]}, format='json')
+        run.lap_set.filter(id=1).update(created_at=idle, run_id=run.id, annotation_id=anno.id)
+
+        self.client.patch(url, {'labels': []}, format='json')
         run = Run.objects.all()
         lap = Lap.objects.all()
 
