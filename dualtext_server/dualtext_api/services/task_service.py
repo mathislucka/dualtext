7fa4cd2c4be5503d95e951f1a68a7b44c7d9417f
mathis <mathis.lucka@gmail.com>
2021-04-28 23:52:10 +0200

    feat: annotation groups shall be copied for review tasks

--- dualtext_server/dualtext_api/services/task_service.py
+++ dualtext_server/dualtext_api/services/task_service.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from dualtext_api.models import Task, Annotation
+from dualtext_api.models import Task, Annotation, AnnotationGroup
 
 class TaskService():
     """
@@ -9,7 +9,7 @@ class TaskService():
         self.task_properties_to_copy = [
             ''
         ]
-    
+
     def copy_task(self, task_id, action=Task.REVIEW):
         task = Task.objects.get(id=task_id)
         copies = task.task_set.all()
@@ -18,11 +18,21 @@ class TaskService():
         task_copy.save()
         self.copy_task_annotations(task, task_copy, action)
         return Task.objects.get(id=task_copy.id)
-    
+
     def copy_task_annotations(self, task, task_copy, action):
         annotations = task.annotation_set.all()
+        group_group_copy_map = {}
         for annotation in annotations:
             annotation_copy = Annotation(task=task_copy, copied_from=annotation, action=action)
+            if annotation.annotation_group:
+                original_group_id = annotation.annotation_group.id
+                group = group_group_copy_map.get(original_group_id, None)
+                if group is None:
+                    group = AnnotationGroup(task=task_copy)
+                    group.save()
+                    group_group_copy_map[original_group_id] = group
+                annotation_copy.annotation_group = group
+
             annotation_copy.save()
             annotation_copy.documents.set(annotation.documents.all())
             annotation_copy.save()
